7af772b1d372a55442057741dc33ff27e2f3be23
racket99 <racket99@gmail.com>
2018-11-15 15:32:42 -0500

    Removed file_to_be_removed

--- pages/views.py
+++ pages/views.py
@@ -7,4 +7,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePageView(request):
-	return HttpResponse('Hello World!')
+	return HttpResponse('Hello, World!  My precious')
